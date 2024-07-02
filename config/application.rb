446a6e6ae45055957af562f890d6f05cf72f8566
Akhileshwar Reddy <akhil.vedas@outlook.com>
2024-07-02 19:29:28 +0530

    Update application.rb with dotenv loading logic

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Railtie.load
+end
+
 module BlogApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
